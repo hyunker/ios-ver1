01a27acf3ec39cd305e83ada9080efad8c7c7cfe
hyunker <slp51074@naver.com>
2020-08-22 23:58:40 +0900

    0822 스크롤 크기변경, 홈화면 틀 완성, 메인광고 수직스크롤수정 근데 짤려서나옴

--- Helloworld/ViewController.swift
+++ Helloworld/ViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 
 
 
-class ViewController: UIViewController, UIGestureRecognizerDelegate {
+class ViewController: UIViewController, UIGestureRecognizerDelegate, UIScrollViewDelegate {
     
     @IBOutlet var homescrollview: UIScrollView!
     
-    @IBOutlet var scrollview: UIScrollView!
-    @IBOutlet var pagecontrol: UIPageControl!
+    @IBOutlet weak var scrollview: UIScrollView!
+    @IBOutlet weak var pagecontrol: UIPageControl!
     @IBAction func astatic_btn(_ sender: Any) {
     }
     @IBAction func wax_btn(_ sender: Any) {
@@ -28,38 +28,37 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBAction func nail_btn(_ sender: Any) {
     }
 
+    var images = ["jpg2.jpg", "png1.png", "invalidName.jpg"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 뒤로가기 제스쳐
         self.navigationController?.interactivePopGestureRecognizer?.delegate = self
         
-        let images = ["jpg2.jpg", "png1.png", "invalidName.jpg"]
-        for i in 0..<images.count{
-            let imageView = UIImageView()
-            imageView.image = UIImage(named: images[i])
-            imageView.contentMode = .scaleAspectFit //  사진의 비율을 맞춤.
-            let xPosition = self.view.frame.width * CGFloat(i)
+        super.viewDidLoad()
+            scrollview.delegate = self
+            pagecontrol.numberOfPages = images.count
+            pagecontrol.currentPage = 0
+            for i in 0..<images.count {
+                let imageview = UIImageView()
+                imageview.image = UIImage(named: images[i])
+                imageview.contentMode = .scaleAspectFit //  사진의 비율을 맞춤.
+                let xPosition = self.view.frame.width * CGFloat(i) // 현재 보이는 스크린에서 하나의 이미지만 보이게 하기 위해
             
-            imageView.frame = CGRect(x: xPosition, y: 0,
-            width: self.view.frame.width,
-            height: self.view.frame.height) // 즉 이미지 뷰가 화면 전체를 덮게 됨.
-            
-            scrollview.contentSize.width =
-               self.view.frame.width * CGFloat(1+i)
-        
-            scrollview.addSubview(imageView)
+                imageview.frame = CGRect(x: xPosition, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+                // 즉 이미지 뷰가 화면 전체를 덮게 됨.
+                scrollview.contentSize.width = self.view.frame.width * CGFloat(1+i)
+                // 이미지의 사이즈를 파악하고 해당 이미지를 올릴 수 있는 만큼 scroll view의 크기를 늘린 후
+                scrollview.addSubview(imageview)
+                // 이미지를 scroll view에 붙인다.
+                
+                // 이미지의 너비가 얼마일지, 이미지가 몇개가 올라갈지 모르기 때문에 그러한 것들을 파악한 후 그에 맞춰 scroll view의 너비를 결정하는 것.
+            }
+        self.view.bringSubviewToFront(pagecontrol)
         }
-        // scrollView에서 페이징이 가능하도록 설정
-        scrollview.isPagingEnabled = true
-        // scrollView의 contentSize를 5 페이지 만큼으로 설정
-        scrollview.contentSize = CGSize(
-            width: UIScreen.main.bounds.width * 3,
-            height: UIScreen.main.bounds.height
-        )
-        scrollview.alwaysBounceHorizontal = true // 수직 스크롤 바운스 안되게 설정
         
-        
-        pagecontrol.numberOfPages = 3
-    }
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let currentPage = round(scrollView.contentOffset.x / self.view.frame.width)
+            pagecontrol.currentPage = Int(CGFloat(currentPage))
+        }
 }
